412b12f17f9e2776f283d93e5e09c2b90030b650
Rafael Mendes <rafaelmendes0606@gmail.com>
2023-09-03 00:08:03 -0300

    ajusting some errors

--- Nexus.Api/Nexus.Api.Domain/Entities/Skill.cs
+++ Nexus.Api/Nexus.Api.Domain/Entities/Skill.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Nexus.Api.Domain.Entities
 {
